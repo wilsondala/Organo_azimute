05e19f73ea580bfb3ba821ef25737677277f92fa
Wilson Dala <wilson.dala@azimute.med.br>
2022-08-29 23:20:11 -0300

    novo_projeto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Banner from "./components/Banner/Banner";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    
+    </div>
+  );
+}
+
+
+export default App;
